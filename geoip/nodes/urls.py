e850ef8a4909b25cf040a5b573aeff03668d6cd1
Jan-Jelle Kester <janjelle@jjkester.nl>
2015-07-23 13:34:38 +0200

    Add map with all nodes

--- geoip/nodes/urls.py
+++ geoip/nodes/urls.py
@@ -2,11 +2,12 @@
 URLs for the GeoIP nodes app.
 """
 from django.conf.urls import url
-from geoip.nodes.views import NodeListView, NodeDetailView, NodeView
+from geoip.nodes.views import NodeListView, NodeDetailView, NodeView, NodeMapView
 
 urlpatterns = [
     url(r'^$', NodeView.as_view(), name='index'),
     url(r'^list/$', NodeListView.as_view(), name='node_list'),
     url(r'^list/(?P<page>\d+)/$', NodeListView.as_view(), name='node_list'),
     url(r'^show/(?P<hashid>\w+)/$', NodeDetailView.as_view(), name='node_detail'),
+    url(r'^map/$', NodeMapView.as_view(), name='node_map'),
 ]
--- geoip/nodes/views.py
+++ geoip/nodes/views.py
@@ -32,6 +32,8 @@ class NodeListView(ListView):
     def get_context_data(self, **kwargs):
         context = super(NodeListView, self).get_context_data(**kwargs)
         context['all_nodes'] = Node.objects.all()
+        context['page_numbers'] = [1, context['paginator'].num_pages]
+        context['page_numbers'].extend([n for n in context['paginator'].page_range if abs(context['page_obj'].number - n) < 5])
         return context
 
 
@@ -46,4 +48,18 @@ class NodeDetailView(HashidsSingleObjectMixin, DetailView):
         context = super(NodeDetailView, self).get_context_data(**kwargs)
         context['datasets'] = Dataset.objects.filter(
             measurements__in=self.object.measurements.all()).distinct().order_by('-start')[:10]
+        return context
+
+
+class NodeMapView(ListView):
+    """
+    Displays nodes on a map.
+    """
+    template_name_suffix = '_map'
+    context_object_name = 'nodes'
+    queryset = Node.objects.active().usable()
+
+    def get_context_data(self, **kwargs):
+        context = super(NodeMapView, self).get_context_data(**kwargs)
+        context['points'] = self.object_list.values_list('location', flat=True)
         return context
